6538bab8b5ff2854738695551d62fa814da8c40c
nnss <matispalomec@gmail.com>
2020-08-17 21:54:35 -0300

    added .gitignore, added widnows support, added config page.

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ This is a simple Flack python script, by now it only contains 1 page. This
 shows the result of the ping checks, getting the data from a python DB
 (json format), and showing a simple graph of the last day.
 
-The ping is done with ApScheduler to 8.8.8.8 and to my router (172.27.1.1).
- This should be configurable, but by now is just hardcoded.
+The ping is done with ApScheduler to and is configured via /config URL. It
+can be 1 or more IPs and each one is showed at the home page.
+
+Now works on windows and *nix. 
 
 This is intended to be really simple script, check 1 or 2 hosts at most. The
 reason for me to do this is to monitor the internet connection. I checked
@@ -21,9 +23,6 @@ dependencies and the DB to store data).
 * add inline doc for auto-document the script
 * improve HTML
   * use templates to improve look and feel
-  * make another page to view all data
-  * add a stop reload in the graphic page (or something in the GET)
-* make configurable via web (store data also in the DB)
-* (opt) decide if this should be just 1 script and make the DB
-  in memory and lost historical data or add some parameters to
-  view historical data.
+  * enhance the navigation menu
+* store IPs in the DB?
+* generate the Dockerfile to build the docker container
--- app.py
+++ app.py
@@ -2,15 +2,18 @@ import datetime
 import os
 import re
 import subprocess
+import platform
 import sys
 from functools import wraps
 from urllib.error import URLError
 from urllib.request import urlopen
 
 import time
-from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask, send_from_directory
+from apscheduler.schedulers.background import BackgroundScheduler, BaseScheduler
+#from flask import Flask, send_from_directory, redirect
+import flask
 from pydblite import Base
+from werkzeug.exceptions import BadRequestKeyError
 
 
 class Config(object):
@@ -18,7 +21,7 @@ class Config(object):
 
 
 class PingMon:
-    def __init__(self, dbh: Base, debug: object = True, ips: [] = ['8.8.8.8', '172.27.1.1']) -> None:
+    def __init__(self, dbh: Base, debug: object = True, ips: [] = list()) -> None:
         """ The initialization gets:
         * dbh (Base object)
         * debug
@@ -30,14 +33,24 @@ class PingMon:
         self.c_error = ''
         self.ips = ips
         self.db = dbh
-        self.cmd = 'ping -c1'
+        param = '-n 1' if platform.system().lower() == 'windows' else '-c1'
+        self.cmd = 'ping' + " " + param
+
+    def add_ping_host(self, host):
+        self.ips.append(host)
+        return self
 
     def jobs(self):
         """ For each IP defined runs the command (ping -c1)."""
+        if len(self.ips) == 0:
+            return self
         for ip in self.ips:
             dt = datetime.datetime.now()
             my_out = self.job(self.cmd + " " + ip)
-            search = re.search(r'.*ttl=\d+\s+time=(\S+)\s.*', my_out, re.MULTILINE)
+            if platform.node():
+                search = re.search(r'.*Media = (\d+)ms.*', my_out, re.MULTILINE)
+            else:
+                search = re.search(r'.*ttl=\d+\s+time=(\S+)\s.*', my_out, re.MULTILINE)
             if search:
                 self.save(ip, search.group(1), str(dt))
             else:
@@ -55,12 +68,17 @@ class PingMon:
         # self.pd("Inside JOB!")
         my_cmd = cmd.split(' ')
         # self.pd("proc " + str(my_cmd))
+        #print("cmd is " + my_cmd)
         output, error = subprocess.Popen(my_cmd,
                                          shell=False,
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE).communicate()
-        output = output.decode('utf-8').strip()
+        try:
+            output = output.decode('utf-8').strip()
+        except UnicodeDecodeError:
+            output = str(output).strip()
         # self.pd(output)
+        print("OUTPUT " + str(output))
         if output == "":
             sys.stderr.write("ERROR: %s" % error)
             self.c_error = error
@@ -110,8 +128,8 @@ class PingMon:
             print("---" + str(msg))
 
 
-app = Flask(__name__)
-db = Base('db/ping.pdl')
+app = flask.Flask(__name__)
+db = Base('db_ping.pdl')
 
 if db.exists():
     db.open()
@@ -125,26 +143,59 @@ sched = BackgroundScheduler()
 job = sched.add_job(pm.jobs, 'interval', minutes=1)
 sched.start()
 
+def html_headers():
+    return """<html>
+    <head><title>ping-mon</title>
+     <meta http-equiv="refresh" content="30">
+    <script src="/js/jquery-1.12.1.min.js"></script>
+    <script src="/js/highcharts.js"></script>
+    <script src="/js/highcharts-more.js"></script>
+    <script src="/js/exporting.js"></script>
+    <script src="/js/data.js"></script>
+    <script src="/js/export-data.js"></script>
+    <style>
+.rounded-corner{font-family:"Lucida Sans Unicode", "Lucida Grande",
+    Sans-Serif;font-size:12px;width:480px;text-align:left;
+    border-collapse:collapse;margin:20px;}
+.rounded-corner thead th.rounded-company{background:#b9c9fe ;}
+.rounded-corner thead th.rounded-q4{background:#b9c9fe ;}
+.rounded-corner th{font-weight:normal;font-size:13px;color:#039;background:#ff7f00;padding:8px;}
+.rounded-corner td{background:#ffd4aa;border-top:1px solid #fff;color:#669;padding:8px;}
+.rounded-corner tfoot td.rounded-foot-left{background:#e8edff ; }
+.rounded-corner tfoot td.rounded-foot-right{background:#e8edff ; }
+.rounded-corner tbody tr:hover td{background:#d0dafd;}
+table tr:first-child th:first-child, table.Info tr:first-child td:first-child { border-top-left-radius: 16px; }
+table tr:first-child th:last-child, table.Info tr:first-child td:last-child { border-top-right-radius: 16px; }
+table tr:last-child td:first-child { border-bottom-left-radius: 16px; }
+table tr:last-child td:last-child { border-bottom-right-radius: 16px; }
+  </style>
+    """
+
+
+def html_footer():
+    return """
+    <nav><ul><li><a href="/config">config</a></li><li><a href="/">Home</a></li>
+    <li><a href="/status">status</a></li></ul></nav>
+    </body></html>
+    """
+
 
 @app.route('/')
-def hello_world():
+def home():
     carry = ""
+    if pm.ips is None:
+        return flask.redirect("/config")
     for ip in pm.ips:
         carry += "<li><a href='/by_host/" + ip + "'>by host " + ip + "</a></li>"
 
-    return """
-<html>
-<head><title>SimpleMonPing</title>
-</head>
+    return html_headers() + """
 <body>
 <h1>Options</h1>
 <ul>
 """ + carry + """
 </ul>
-<footer>""" + str(sched.state) + " " + str(sched.get_jobs()) + """</footer>
-</body>
-</html>    
-"""
+<footer>""" + str(sched.state) + " " + str(sched.get_jobs()) + """</footer>  
+""" + html_footer()
 
 
 @app.route('/by_host/<ip>')
@@ -164,16 +215,7 @@ def by_host(ip='8.8.8.8'):
             # carry += '<tr><td>' + str(result['host']) + '</td><td>' + str(result['datetime']) + '</td><td>' + \
             #           str(result['result']) + '</td></tr>'
             carry += '<tr><td>' + str(result['datetime']) + '</td><td>' + str(result['result']) + '</td></tr>'
-    ret = """
-    <html>
-    <head><title>SimpleMonPing</title>
-     <meta http-equiv="refresh" content="30">
-    <script src="/js/jquery-1.12.1.min.js"></script>
-    <script src="/js/highcharts.js"></script>
-    <script src="/js/highcharts-more.js"></script>
-    <script src="/js/exporting.js"></script>
-    <script src="/js/data.js"></script>
-    <script src="/js/export-data.js"></script>
+    ret = html_headers() + """
     <script type='text/javascript'>
 //<![CDATA[
 $(document).ready(function(){
@@ -239,22 +281,6 @@ $(function () {
 });
 //]]
   </script>
-    <style>
-.rounded-corner{font-family:"Lucida Sans Unicode", "Lucida Grande",
-    Sans-Serif;font-size:12px;width:480px;text-align:left;
-    border-collapse:collapse;margin:20px;}
-.rounded-corner thead th.rounded-company{background:#b9c9fe ;}
-.rounded-corner thead th.rounded-q4{background:#b9c9fe ;}
-.rounded-corner th{font-weight:normal;font-size:13px;color:#039;background:#ff7f00;padding:8px;}
-.rounded-corner td{background:#ffd4aa;border-top:1px solid #fff;color:#669;padding:8px;}
-.rounded-corner tfoot td.rounded-foot-left{background:#e8edff ; }
-.rounded-corner tfoot td.rounded-foot-right{background:#e8edff ; }
-.rounded-corner tbody tr:hover td{background:#d0dafd;}
-table tr:first-child th:first-child, table.Info tr:first-child td:first-child { border-top-left-radius: 16px; }
-table tr:first-child th:last-child, table.Info tr:first-child td:last-child { border-top-right-radius: 16px; }
-table tr:last-child td:first-child { border-bottom-left-radius: 16px; }
-table tr:last-child td:last-child { border-bottom-right-radius: 16px; }
-  </style>
     </head>
     <body>
     <h1>list for """ + ip + """</h1>
@@ -270,16 +296,42 @@ table tr:last-child td:last-child { border-bottom-right-radius: 16px; }
     <footer>Schedule state: """ + str(sched.state) + \
           "<br /> Last updated:" + str(datetime.datetime.now().isoformat()) + """<br />
           Stats LOST: """ + "" + """
-    </footer>
-    </body>
-    </html>    
-    """
+    </footer>   
+    """ + html_footer()
     return ret
 
 
 @app.route('/js/<path:path>')
 def send_js(path):
-    return send_from_directory('static', path)
+    return flask.send_from_directory('static', path)
+
+
+@app.route("/status")
+def status():
+    global sched
+    return html_headers() + "</head></body><h1>Status</h1><pre>" + str(sched.get_jobs()) + "</pre>" + html_footer()
+
+
+@app.route("/config/", methods=['GET', 'POST'])
+def general_config():
+    global job
+    ip = flask.request.args.get("ip", None)
+    if ip == "":
+        ip = None
+    if ip is not None:
+        if ip not in pm.ips:
+            pm.add_ping_host(ip)
+            job.remove()
+            job = sched.add_job(pm.jobs, 'interval', minutes=1)
+            #sched.shutdown()
+            #sched.start()
+            print(sched.state)
+        return flask.redirect("/")
+    if pm.ips is not None and ip is not None and ip in pm.ips:
+        return html_headers() + """</head><body><h1>IP already added</h1></body></html>"""
+    return html_headers() + """</head><body><h1>Add IP to monitor</h1>
+    <form action="/config" method="get"><label>ip</label><input type="text" name="ip" /><input type="submit">
+    """ + html_footer()
 
 
 if __name__ == "__main__":
